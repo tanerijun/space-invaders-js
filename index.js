49c5e4be4a7be5513a0cd882a96c4fc57cd3d5c3
tanerijun <tanerivince@gmail.com>
2024-08-28 22:51:59 +0800

    Create player

--- index.js
+++ index.js
@@ -12,24 +12,52 @@ window.addEventListener("resize", fullScreenCanvas);
 
 class Player {
   constructor() {
-    this.position = {
-      x: 200,
-      y: 200,
-    };
     this.velocity = {
       x: 0,
       y: 0,
     };
-    // this.image
-    this.width = 100;
-    this.height = 100;
+
+    const image = new Image();
+    image.src = "assets/image/spaceship.png";
+    image.onload = () => {
+      const scale = 0.15;
+      this.image = image;
+      this.width = this.image.width * scale;
+      this.height = this.image.height * scale;
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height - this.height - 20,
+      };
+    };
   }
 
   draw() {
-    ctx.fillStyle = "red";
-    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    if (!this.image) {
+      return;
+    }
+    // ctx.fillStyle = "red";
+    // ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
   }
 }
 
-const player = new Player();
-player.draw();
+class Game {
+  constructor() {
+    this.player = new Player();
+
+    this.loop = this.loop.bind(this);
+  }
+
+  loop() {
+    requestAnimationFrame(this.loop);
+
+    // Set background to black
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    this.player.draw();
+  }
+}
+
+const game = new Game();
+game.loop();
