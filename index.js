e420f6bacb2c4ffebd1bd3543c9f6a34851248dd
tanerijun <tanerivince@gmail.com>
2024-08-28 23:44:06 +0800

    Player movement

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ class Player {
       x: 0,
       y: 0,
     };
+    this.speed = 5;
+    this.rotation = 0;
 
     const image = new Image();
     image.src = "assets/image/spaceship.png";
@@ -31,19 +33,82 @@ class Player {
     };
   }
 
-  draw() {
+  #draw() {
+    ctx.save();
+
+    // Rotate player (for tilt effect)
+    ctx.translate(this.position.x + this.width / 2, this.position.y + this.height / 2); // makes the middle of player (0, 0)
+    ctx.rotate(this.rotation);
+    ctx.translate(-this.position.x - this.width / 2, -this.position.y - this.height / 2); // revert to original coord system
+
+    ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    ctx.restore();
+  }
+
+  update() {
     if (!this.image) {
       return;
     }
-    // ctx.fillStyle = "red";
-    // ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    this.#draw();
+    this.position.x += this.velocity.x;
+  }
+
+  moveLeft() {
+    this.velocity.x = -this.speed;
+    this.rotation = -0.15;
+  }
+
+  moveRight() {
+    this.velocity.x = this.speed;
+    this.rotation = 0.15;
+  }
+
+  stop() {
+    this.velocity.x = 0;
+    this.rotation = 0;
   }
 }
 
 class Game {
   constructor() {
     this.player = new Player();
+    this.playerSpeed = 5;
+    this.keys = {
+      ArrowLeft: {
+        pressed: false,
+      },
+      ArrowRight: {
+        pressed: false,
+      },
+    };
+
+    window.addEventListener("keydown", ({ key }) => {
+      switch (key) {
+        case "ArrowLeft":
+          this.keys.ArrowLeft.pressed = true;
+          break;
+        case "ArrowRight":
+          this.keys.ArrowRight.pressed = true;
+          break;
+        case "z":
+          console.log("z");
+          break;
+      }
+    });
+
+    window.addEventListener("keyup", ({ key }) => {
+      switch (key) {
+        case "ArrowLeft":
+          this.keys.ArrowLeft.pressed = false;
+          break;
+        case "ArrowRight":
+          this.keys.ArrowRight.pressed = false;
+          break;
+        case "z":
+          console.log("z");
+          break;
+      }
+    });
 
     this.loop = this.loop.bind(this);
   }
@@ -55,7 +120,20 @@ class Game {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    this.player.draw();
+    this.player.update();
+
+    const bothKeysArePressed = this.keys.ArrowLeft.pressed && this.keys.ArrowRight.pressed;
+    if (!bothKeysArePressed && this.keys.ArrowLeft.pressed && this.player.position.x >= 0) {
+      this.player.moveLeft();
+    } else if (
+      !bothKeysArePressed &&
+      this.keys.ArrowRight.pressed &&
+      this.player.position.x + this.player.width <= canvas.width
+    ) {
+      this.player.moveRight();
+    } else {
+      this.player.stop();
+    }
   }
 }
 
